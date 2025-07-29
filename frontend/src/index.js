fb8ea7c03a6d0c553eb6608abe6917111b6448a5
claudiajerome07 <claudiajerome07@gmail.com>
2025-07-29 14:37:56 +0530

    building the container

---
+++ frontend/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app'; // ✅ this is your custom App.js
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
